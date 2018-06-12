4db822f079d133833bac4873b95367ee5b7392bf
tyuen11 <timothy.yuen11@gmail.com>
2018-06-11 22:22:26 -0400

    first push

---
+++ todo project/app.js
@@ -0,0 +1,23 @@
+function newItem(){
+  var newTask = document.getElementById("taskcreator").value;
+  var list = document.getElementById("list");
+  var item = document.createElement("item");
+
+  item.appendChild(document.createTextNode("-" + newTask));
+  list.appendChild(item);
+  document.getElementById("taskcreator").value = "";
+
+  item.onclick = removeItem;
+}
+
+<!--adds item to list when enter key is pressed-->
+document.body.onkeyup = function(e){
+  if (e.keyCode == 13){
+    newItem();
+  }
+}
+
+function removeItem(e){
+  e.target.parentElement.removeChild(e.target);
+  <!--parentElement is the list "list"-->
+}
